eb624281c5816eba31961dba2a6f153d091783e9
Hendoe <hendo1349@gmail.com>
2020-09-18 13:44:23 -0500

    initial

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const MOVIES = require('./movies-data-small.json')
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const MOVIES = require('./movies-data-small.json');
 
-const app = express()
+const app = express();
 
-app.use(morgan('dev'))
-app.use(cors())
-app.use(helmet())
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+app.use(morgan(morganSetting));
+app.use(cors());
+app.use(helmet());
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN
@@ -18,7 +19,7 @@ app.use(function validateBearerToken(req, res, next) {
     return res.status(401).json({ error: 'Unauthorized request' })
   }
   next()
-})
+});
 
 app.get('/movie', function handleGetMovie(req, res) {
   let response = MOVIES;
@@ -42,10 +43,20 @@ app.get('/movie', function handleGetMovie(req, res) {
   }
 
   res.json(response)
-})
+});
 
-const PORT = 9000
+// 4 parameters in middleware, express knows to treat this as error handler
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+});
+
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
-})
+});
